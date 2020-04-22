c32055b44ba9d44675fdd92eedda31f00d83c319
sunnysunita <sunnysunita.com@gmail.com>
2020-04-22 18:52:37 +0530

    final

---
+++ remove leaf.py
@@ -0,0 +1,80 @@
+import queue
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def nodesWithoutSibling(root):
+    # Given a binary tree, print all nodes that don’t have a sibling. Print the
+    # elements in different lines. And order of elements doesn't matter.
+    #############################
+    # PLEASE ADD YOUR CODE HERE #
+    #############################
+    if root is None:
+        return None
+    left_tree = nodesWithoutSibling(root.left)
+    right_tree = nodesWithoutSibling(root.right)
+    if left_tree is None and right_tree is True:
+        print(root.right.data)
+        return True
+    elif left_tree is True and right_tree is None:
+        print(root.left.data)
+        return True
+    elif left_tree is None and right_tree is None:
+        return True
+    elif right_tree is True and left_tree is True:
+        return True
+
+def remove_leaf(root):
+    if root is None:
+        return None
+    if root.left is None and root.right is None:
+        return None
+    root.left = remove_leaf(root.left)
+    root.right = remove_leaf(root.right)
+    return root
+def preOrder(root):
+    # Given a binary tree, print the preorder traversal of given tree. Pre-order
+    # traversal is: Root LeftChild RightChild
+    #############################
+    # PLEASE ADD YOUR CODE HERE #
+    #############################
+    if root is None:
+        return
+    print(root.data, end=" ")
+    preOrder(root.left)
+    preOrder(root.right)
+def buildLevelTree(levelorder):
+    index = 0
+    length = len(levelorder)
+    if length<=0 or levelorder[0]==-1:
+        return None
+    root = BinaryTreeNode(levelorder[index])
+    index += 1
+    q = queue.Queue()
+    q.put(root)
+    while not q.empty():
+        currentNode = q.get()
+        leftChild = levelorder[index]
+        index += 1
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+        rightChild = levelorder[index]
+        index += 1
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+    return root
+
+# Main
+levelOrder = [int(i) for i in input().strip().split()]
+root = buildLevelTree(levelOrder)
+preOrder(root)
+print()
+root = remove_leaf(root)
+
+preOrder(root)
