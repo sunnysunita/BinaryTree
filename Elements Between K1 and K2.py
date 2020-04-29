78cd0dd9daf5b38ee5a8a70429d4df5a2fe72a73
sunnysunita <sunnysunita.com@gmail.com>
2020-04-29 17:06:59 +0530

    final

---
+++ Elements Between K1 and K2.py
@@ -0,0 +1,55 @@
+import queue
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def elementsInRangeK1K2(root, k1, k2, list):
+    if root is None:
+        return list
+    if root.data > k2:
+        elementsInRangeK1K2(root.left, k1, k2, list)
+    if root.data < k1:
+        elementsInRangeK1K2(root.right, k1, k2, list)
+    else:
+        # print(root.data, end=" ")
+        list.append(root.data)
+        elementsInRangeK1K2(root.left, k1, k2, list)
+        elementsInRangeK1K2(root.right, k1, k2, list)
+    return list
+
+
+def buildLevelTree(levelorder):
+    index = 0
+    length = len(levelorder)
+    if length<=0 or levelorder[0]==-1:
+        return None
+    root = BinaryTreeNode(levelorder[index])
+    index += 1
+    q = queue.Queue()
+    q.put(root)
+    while not q.empty():
+        currentNode = q.get()
+        leftChild = levelorder[index]
+        index += 1
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+        rightChild = levelorder[index]
+        index += 1
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+    return root
+
+# Main
+levelOrder = [int(i) for i in input().strip().split()]
+root = buildLevelTree(levelOrder)
+k1, k2 = (int(i) for i in input().strip().split())
+list1 = elementsInRangeK1K2(root, k1, k2, list=[])
+list1.sort()
+for e in list1:
+    print(e, end=" ")
